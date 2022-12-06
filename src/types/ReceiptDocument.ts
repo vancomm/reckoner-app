9290bb722bd5c8e1140e55369d6ff67cde87c05e
vancomm <peshekhonov.ip@gmail.com>
2022-12-06 03:49:33 +0300

    Fix: modified JSON schema and type definition to ignore irrelevant properies

--- src/types/ReceiptDocument.ts
+++ src/types/ReceiptDocument.ts
@@ -3,36 +3,7 @@ type ReceiptDocument = ReceiptDocumentItem[];
 export default ReceiptDocument;
 
 export interface ReceiptDocumentItem {
-  status: number;
-  seller: Organization;
-  operation: Operation;
-  id: string;
-  organization: Organization;
-  qr: string;
-  query: Query;
   ticket: Ticket;
-  kind: string;
-  createdAt: number;
-}
-
-export interface Operation {
-  date: number;
-  type: number;
-  sum: number;
-}
-
-export interface Organization {
-  name: string;
-  inn: string;
-}
-
-export interface Query {
-  date: string;
-  documentId: number;
-  fsId: string;
-  fiscalSign: string;
-  operationType: number;
-  sum: number;
 }
 
 export interface Ticket {
@@ -44,31 +15,7 @@ export interface Document {
 }
 
 export interface Receipt {
-  retailPlace: string;
-  totalSum: number;
-  operator: string;
-  userInn: string;
   items: Item[];
-  code: number;
-  ecashTotalSum: number;
-  requestNumber: number;
-  fiscalDriveNumber: string;
-  shiftNumber: number;
-  kktRegId: string;
-  provisionSum: number;
-  nds18: number;
-  dateTime: number;
-  retailPlaceAddress: string;
-  fiscalSign: number;
-  user: string;
-  prepaidSum: number;
-  cashTotalSum: number;
-  fiscalDocumentNumber: number;
-  operationType: number;
-  fiscalDocumentFormatVer: number;
-  creditSum: number;
-  taxationType: number;
-  nds10: number;
 }
 
 export interface Item {
